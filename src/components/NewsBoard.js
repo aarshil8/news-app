88e0f983fab92d344d31dadc2f5c3685591db2f5
aarshil8 <aarshiljd@gmail.com>
2024-04-18 18:12:53 +0530

    Discover the pulse of the world with our React-powered News Explorer app. Delve into a myriad of categories including business, technology, sports, health, and entertainment, all in one place.

---
+++ src/components/NewsBoard.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.REACT_APP_MY_KEY}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, [category]);
+
+  return (
+    <div>
+      <h2 className="text-center my-2">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NewsBoard;
